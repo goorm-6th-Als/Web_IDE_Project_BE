eeed672c9cbafb799d2b14d66f08ee77a63b0768
남수연 <53739820+namtndus@users.noreply.github.com>
2024-05-14 10:51:11 +0900

    [feat]: Jwtfilter 와 Jwtprovider 완성

--- src/main/java/com/als/webIde/domain/repository/MemberRepository.java
+++ src/main/java/com/als/webIde/domain/repository/MemberRepository.java
@@ -4,8 +4,7 @@ import com.als.webIde.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
-public interface MemberRepositpory extends JpaRepository<Member,Long> {
+public interface MemberRepository extends JpaRepository<Member,Long> {
     List<Member> findMemberByUserId(String UserId);
 }
